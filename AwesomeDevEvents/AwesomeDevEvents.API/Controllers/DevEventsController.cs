714cd951d8779179c3be17288a9d93d7ee9585e5
vascsx <vasczn@gmail.com>
2023-10-23 21:28:22 -0300

    first commit

---
+++ AwesomeDevEvents/AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/[dev-events]")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+
+
+    }
+}
